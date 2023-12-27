fe11cd723d92b42e7afbb0ed43374fec42d146bc
Ha Thi <ha.pham25@student.passerellesnumeriques.org>
2023-12-27 21:58:47 +0700

    MVC

---
+++ views/home/index.view.php
@@ -0,0 +1,14 @@
+<?php require "partials/head.php" ?>
+<?php require "partials/nav.php"?>
+
+
+  <main>
+    <div class="p-3">
+      <!-- Replace with your content -->
+     <p>Hello Home</p>
+     <h1>HELLO WORLD</h1>
+      <!-- /End replace -->
+    </div>
+  </main>
+  
+<?php require "partials/footer.php" ?>
